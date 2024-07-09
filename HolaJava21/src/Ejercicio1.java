6cee75e122e163dd000883009639f1336ab18aca
Carlos Saltos <csaltos@gmail.com>
2024-07-09 05:52:11 -0300

    Ejercicio 1

--- HolaJava21/src/Ejercicio1.java
+++ HolaJava21/src/Ejercicio1.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class Ejercicio1 {
 
     public static void main(String[] args) {
+        var listaNumeros = new ArrayList<Integer>();
         var numeroEnTexto = """
                 1
                 2
@@ -9,11 +12,17 @@ public class Ejercicio1 {
                 20
                 80
                 -100""";
-        var flujo = numeroEnTexto.lines();
-        var resultado = flujo
-                .map(Integer::parseInt)
-                .filter(i -> i >= 0).toList();
+        var lista = numeroEnTexto.lines().toList();
+        var resultado = lista.stream()
+                .map(s -> {
+                    var i = Integer.parseInt(s);
+                    listaNumeros.add(i);
+                    return i;
+                })
+                //.filter(i -> i >= 0)
+                .count();
         System.out.println("Resultado: " + resultado);
+        System.out.println("listaNumeros: " + listaNumeros);
     }
 
 }
