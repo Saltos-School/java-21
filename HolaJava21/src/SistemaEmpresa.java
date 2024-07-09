88c87faedeaad8c34c77c2ae1608da958bb6f0b0
Carlos Saltos <csaltos@gmail.com>
2024-07-09 08:55:55 -0300

    Sistema empresa

--- HolaJava21/src/SistemaEmpresa.java
+++ HolaJava21/src/SistemaEmpresa.java
@@ -1,90 +1,56 @@
-enum TipoEmpleado {
-    FIJO,
-    AUTONOMO,
-    BECARIO,
-    MEDIO_TIEMPO,
-    TIEMPO_EXTRA
-}
-
-interface Empleado {
+sealed interface Empleado permits Fijo, Autonomo, Becario, MedioTiempo, TiempoExtra {
     String nombre();
-    TipoEmpleado tipoEmpleado();
 }
 
-class Fijo implements Empleado {
+final class Fijo implements Empleado {
     public String nombre() {
         return "Pepe Fijo";
     }
-
-    @Override
-    public TipoEmpleado tipoEmpleado() {
-        return TipoEmpleado.FIJO;
-    }
 }
 
-class Autonomo implements Empleado {
+final class Autonomo implements Empleado {
     public String nombre() {
         return "María Autonoma";
     }
-
-    @Override
-    public TipoEmpleado tipoEmpleado() {
-        return TipoEmpleado.AUTONOMO;
-    }
 }
 
-class Becario implements Empleado {
+final class Becario implements Empleado {
     public String nombre() {
         return "Andrés Becario";
     }
-
-    @Override
-    public TipoEmpleado tipoEmpleado() {
-        return TipoEmpleado.BECARIO;
-    }
 }
 
-class MedioTiempo implements Empleado {
+final class MedioTiempo implements Empleado {
     public String nombre() {
         return "Susana Medio Tiempo";
     }
+}
 
-    @Override
-    public TipoEmpleado tipoEmpleado() {
-        return TipoEmpleado.MEDIO_TIEMPO;
+final class TiempoExtra implements Empleado {
+    public String nombre() {
+        return "Juanita Tiempo Extra";
     }
 }
 
 class Pagos {
 
-    public static int calcularMontoAPagar(TipoEmpleado tipoEmpleado) {
-        return switch (tipoEmpleado) {
-            case FIJO -> 5000;
-            case AUTONOMO -> 4000;
-            case BECARIO -> 15000;
-            case MEDIO_TIEMPO -> 7000;
-            case TIEMPO_EXTRA -> 20000;
-        };
-    }
-
     public static void pagar(Empleado empleado) {
         System.out.println("Pagando a " + empleado.nombre());
-        switch (empleado.tipoEmpleado()) {
-            case FIJO -> {
-                var fijo = (Fijo) empleado;
+        switch (empleado) {
+            case Fijo fijo -> {
                 System.out.println("Pago de " + 5000 + " a " + fijo.nombre());
             }
-            case AUTONOMO -> {
-                var autonomo = (Autonomo) empleado;
+            case Autonomo autonomo -> {
                 System.out.println("Pago de " + 4000 + " a " + autonomo.nombre());
             }
-            case BECARIO -> {
-                var becario = (Becario) empleado;
+            case Becario becario -> {
                 System.out.println("Pago de " + 15000 + " a " + becario.nombre());
             }
-            case MEDIO_TIEMPO -> {
-                var medioTiempo = (MedioTiempo) empleado;
+            case MedioTiempo medioTiempo -> {
                 System.out.println("Pago de " + 7000 + " a " + medioTiempo.nombre());
+            }
+            case TiempoExtra tiempoExtra -> {
+                System.out.println("Pago de " + 10000 + " a " + tiempoExtra.nombre());
             }
         }
     }
@@ -98,10 +64,12 @@ public class SistemaEmpresa {
         var autonomo = new Autonomo();
         var becario = new Becario();
         var medioTiempo = new MedioTiempo();
+        var tiempoExtra = new TiempoExtra();
         Pagos.pagar(fijo);
         Pagos.pagar(autonomo);
         Pagos.pagar(becario);
         Pagos.pagar(medioTiempo);
+        Pagos.pagar(tiempoExtra);
     }
 
 }
