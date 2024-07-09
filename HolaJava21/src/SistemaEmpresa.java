2f4f6bee35453b4ae08c40f5f3c46ba00e0b9625
Carlos Saltos <csaltos@gmail.com>
2024-07-09 08:08:09 -0300

    Sistema empresa

--- HolaJava21/src/SistemaEmpresa.java
+++ HolaJava21/src/SistemaEmpresa.java
@@ -1,10 +1,22 @@
+enum TipoEmpleado {
+    FIJO,
+    AUTONOMO,
+    BECARIO
+}
+
 interface Empleado {
     String nombre();
+    TipoEmpleado tipoEmpleado();
 }
 
 class Fijo implements Empleado {
     public String nombre() {
         return "Pepe Fijo";
+    }
+
+    @Override
+    public TipoEmpleado tipoEmpleado() {
+        return TipoEmpleado.FIJO;
     }
 }
 
@@ -12,18 +24,37 @@ class Autonomo implements Empleado {
     public String nombre() {
         return "María Autonoma";
     }
+
+    @Override
+    public TipoEmpleado tipoEmpleado() {
+        return TipoEmpleado.AUTONOMO;
+    }
+}
+
+class Becario implements Empleado {
+    public String nombre() {
+        return "Andrés Becario";
+    }
+
+    @Override
+    public TipoEmpleado tipoEmpleado() {
+        return TipoEmpleado.BECARIO;
+    }
 }
 
 class Pagos {
 
     public static void pagar(Empleado empleado) {
         System.out.println("Pagando a " + empleado.nombre());
-        if (empleado instanceof Fijo) {
-            var fijo = (Fijo) empleado;
+        if (empleado instanceof Fijo fijo) {
             System.out.println("Pago de " + 5000 + " a " + fijo.nombre());
-        } else if (empleado instanceof Autonomo) {
-            var autonomo = (Autonomo) empleado;
+        } else if (empleado instanceof Autonomo autonomo) {
             System.out.println("Pago de " + 4000 + " a " + autonomo.nombre());
+        } else if (empleado instanceof Becario becario) {
+            System.out.println("Pago de " + 15000 + " a " + becario.nombre());
+        } else {
+            throw new IllegalArgumentException(
+                    "Tipo de empleado no soportado " + empleado.getClass().getSimpleName());
         }
     }
 
@@ -34,8 +65,10 @@ public class SistemaEmpresa {
     public static void main(String[] args) {
         var fijo = new Fijo();
         var autonomo = new Autonomo();
+        var becario = new Becario();
         Pagos.pagar(fijo);
         Pagos.pagar(autonomo);
+        Pagos.pagar(becario);
     }
 
 }
