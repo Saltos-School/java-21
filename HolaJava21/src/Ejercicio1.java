44c95295087fef594947b236ae8d0ded940db1f9
Carlos Saltos <csaltos@gmail.com>
2024-07-09 06:04:01 -0300

    Ejercicio 1

--- HolaJava21/src/Ejercicio1.java
+++ HolaJava21/src/Ejercicio1.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-        var listaNumeros = new ArrayList<Integer>();
         var numeroEnTexto = """
                 1
                 2
@@ -13,16 +12,13 @@ public class Ejercicio1 {
                 80
                 -100""";
         var lista = numeroEnTexto.lines().toList();
-        var resultado = lista.stream()
-                .map(s -> {
-                    var i = Integer.parseInt(s);
-                    listaNumeros.add(i);
-                    return i;
-                })
-                //.filter(i -> i >= 0)
+        var intermedio = lista.stream()
+                .map(Integer::parseInt).toList();
+        var resultado = intermedio.stream()
+                .filter(i -> i >= 0)
                 .count();
         System.out.println("Resultado: " + resultado);
-        System.out.println("listaNumeros: " + listaNumeros);
+        System.out.println("listaNumeros: " + intermedio);
     }
 
 }
