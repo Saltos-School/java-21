c7d4a04631ad8cc0cbdc36f89933c924ac58e8a0
Carlos Saltos <csaltos@gmail.com>
2024-07-10 07:50:08 -0300

    Hilos

--- HolaJava21/src/HolaVirtualThreads.java
+++ HolaJava21/src/HolaVirtualThreads.java
@@ -1,4 +1,5 @@
 import java.util.Vector;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class HolaVirtualThreads {
 
@@ -6,8 +7,11 @@ public class HolaVirtualThreads {
         var inicio = System.currentTimeMillis();
         var hilos = new Vector<Thread>(5);
         final int j = 20;
+        final AtomicLong total = new AtomicLong(0);
+        final AtomicLong total2 = new AtomicLong(0);
         for (int i = 0; i < 5; i++) {
             final var mensaje = "Ejecutando hilo numero " + i + " (" + j + ")";
+            final var iCopia = i;
             var thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
@@ -16,6 +20,8 @@ public class HolaVirtualThreads {
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
+                    total.addAndGet(iCopia);
+                    total2.incrementAndGet();
                     System.out.println(mensaje);
                 }
             });
@@ -31,6 +37,8 @@ public class HolaVirtualThreads {
             }
         }
         System.out.println("Continuando main");
+        System.out.println("El total es " + total);
+        System.out.println("El total2 es " + total2);
         var fin = System.currentTimeMillis();
         System.out.printf("Tiempo total %dms", fin - inicio);
     }
