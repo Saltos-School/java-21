050e422cba6e12408a9e070ebbb826a58dbf677b
Carlos Saltos <csaltos@gmail.com>
2024-07-10 06:21:09 -0300

    Sistema empresa

--- HolaJava21/src/SistemaEmpresa.java
+++ HolaJava21/src/SistemaEmpresa.java
@@ -15,7 +15,7 @@ record Becario(String nombre, int edad) implements Empleado {
 record MedioTiempo(String nombre, int edad) implements Empleado {
 }
 
-record TiempoExtra(String nombre, int edad) implements Empleado {
+record TiempoExtra(String nombre, int edad, int totalHorasExtras) implements Empleado {
 }
 
 class Pagos {
@@ -23,7 +23,7 @@ class Pagos {
     public static void pagar(Empleado empleado) {
         System.out.println("Pagando a " + empleado.nombre());
         switch (empleado) {
-            case Fijo(var nombre, _) when nombre.contains("Pepe") -> {
+            case Fijo(var nombre, var _) when nombre.contains("Pepe") -> {
                 System.out.println("Pago de " + 50000 + " a " + nombre);
             }
             case Fijo(String nombre, _) -> {
@@ -35,14 +35,12 @@ class Pagos {
             case Autonomo(var nombre, var edad) -> {
                 System.out.println("Pago de " + 4000 + " a " + nombre + " con edad " + edad);
             }
-            case Becario becario -> {
-                System.out.println("Pago de " + 15000 + " a " + becario.nombre());
+            case Becario _, MedioTiempo _ -> {
+                System.out.println("Pago de " + 15000 + " a " + empleado.nombre());
             }
-            case MedioTiempo medioTiempo -> {
-                System.out.println("Pago de " + 7000 + " a " + medioTiempo.nombre());
-            }
-            case TiempoExtra tiempoExtra -> {
-                System.out.println("Pago de " + 10000 + " a " + tiempoExtra.nombre());
+            case TiempoExtra(var nombre, var _, var totalHorasExtras) -> {
+                var pago = 10000 + totalHorasExtras * 700;
+                System.out.println("Pago de " + pago + " a " + nombre);
             }
         }
     }
@@ -56,7 +54,7 @@ public class SistemaEmpresa {
         var autonomo = new Autonomo("María Autonoma", 20);
         var becario = new Becario("Andrés Becario", 25);
         var medioTiempo = new MedioTiempo("Susana Medio Tiempo", 40);
-        var tiempoExtra = new TiempoExtra("Juanita Tiempo Extra", 30);
+        var tiempoExtra = new TiempoExtra("Juanita Tiempo Extra", 30, 5);
         Pagos.pagar(fijo);
         Pagos.pagar(autonomo);
         Pagos.pagar(becario);
